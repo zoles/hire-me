d6bb1524ef298a632f4d4dd1993c15834f9a6ddc
Zoltan Mehes <mehes.zoltan@gmail.com>
2021-11-28 12:16:21 +0100

    removed dummy test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,10 +2,6 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-it("renders without crashing", () => {
-  render(<App />);
-});
-
 test("renders header text", () => {
   render(<App />);
   const linkElement = screen.getByText(/Checkin and checkout management/i);
